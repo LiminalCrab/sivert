72a72e74ee134eb56c19c668f4f57b5752d1beeb
Preston <78995156+LiminalCrab@users.noreply.github.com>
2022-12-22 23:31:58 -0800

    Created GlobalRunning to satisfy WM_DESTROY and WM_CLOSE

--- win32_sivert.cpp
+++ win32_sivert.cpp
@@ -1,82 +1,90 @@
 #include <windows.h>
 
-
+// Temporary global variable
+static bool GlobalRunning = true;
 
 LRESULT CALLBACK
 MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
     LRESULT Result = 0;
 
-    switch(Message)
+    switch (Message)
     {
-        case WM_SIZE:
-        {
-            OutputDebugStringA("WM_SIZE\n");
-        } break;
-
-        case WM_DESTROY:
-        {
-            OutputDebugStringA("WM_DESTROY\n");
-        } break;
-
-        case WM_CLOSE:
-        {
-            PostQuitMessage(0);
-            OutputDebugStringA("WM_CLOSE\n");
-        } break;
-
-        case WM_ACTIVATEAPP:
-        {
-            OutputDebugStringA("WM_ACTIVATEAPP\n");
-        } break;
-
-        case WM_PAINT:
-        {
-            PAINTSTRUCT Paint;
-            HDC DeviceContext = BeginPaint(Window, &Paint);
-            int X = Paint.rcPaint.left;
-            int Y = Paint.rcPaint.top;
-            LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            static DWORD Operation = WHITENESS;
-            PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
-            EndPaint(Window, &Paint);
-             
-        } break;
-
-        default:
-        {
-           // OutputDebugStringA("default\n");
-           Result = DefWindowProc(Window, Message, WParam, LParam);
-        } break;
-
+    case WM_SIZE:
+    {
+        OutputDebugStringA("WM_SIZE\n");
     }
-        return(Result);
+    break;
+
+    case WM_DESTROY:
+    {
+         // TODO: Message handler to user 
+        // OutputDebugStringA("WM_DESTROY\n");
+        GlobalRunning = false;
+    }
+    break;
+
+    case WM_CLOSE:
+    {
+         // TODO: Message handler to user 
+        // OutputDebugStringA("WM_CLOSE\n");
+        GlobalRunning = false;
+    }
+    break;
+
+    case WM_ACTIVATEAPP:
+    {
+        OutputDebugStringA("WM_ACTIVATEAPP\n");
+    }
+    break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT Paint;
+        HDC DeviceContext = BeginPaint(Window, &Paint);
+        int X = Paint.rcPaint.left;
+        int Y = Paint.rcPaint.top;
+        LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+        LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
+        static DWORD Operation = WHITENESS;
+        PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+        EndPaint(Window, &Paint);
+    }
+    break;
+
+    default:
+    {
+        // OutputDebugStringA("default\n");
+        Result = DefWindowProc(Window, Message, WParam, LParam);
+    }
+    break;
+    }
+    return (Result);
 }
 
 int CALLBACK
 WinMain(HINSTANCE Instance,
-HINSTANCE PrevInstance,
-LPSTR CmdLine,
-int ShowCode)
+        HINSTANCE PrevInstance,
+        LPSTR CmdLine,
+        int ShowCode)
 {
 
     WNDCLASS WindowClass = {};
-    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
     WindowClass.lpfnWndProc = MainWindowCallback;
     WindowClass.hInstance = Instance;
     WindowClass.lpszClassName = "SievertWindowClass";
     // WindowClass.hIcon;
     // WindowClass.hCursor;
-    
-    // What the fuck is an Atom?
-    if(RegisterClass(&WindowClass))
+
+    if (RegisterClass(&WindowClass))
     {
-        HWND WindowHandle = CreateWindowEx(
+        // Create a window.
+        HWND WindowHandle = CreateWindowExA(
             0,
             WindowClass.lpszClassName,
             "Sievert",
-            WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
@@ -85,34 +93,35 @@ int ShowCode)
             0,
             Instance,
             0);
-            if(WindowHandle)
+
+        if (WindowHandle)
+        {
+            MSG Message;
+            for (;;) // infinite for loop
             {
-                MSG Message;
-                for(;;)
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
                 {
-                    BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                    if(MessageResult > 0)
-                    {
-                        // Translate keyboard messages
-                        TranslateMessage(&Message);
-                        DispatchMessage(&Message);
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    // Translate keyboard messages
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    // break out of infinite for loop.
+                    break;
                 }
             }
-            else
-            {   
-                // logging
-            }
+        }
+        else
+        {
+            // logging
+        }
     }
     else
     {
         // logging
     }
 
-    return(0);
+    return (0);
 }
-
